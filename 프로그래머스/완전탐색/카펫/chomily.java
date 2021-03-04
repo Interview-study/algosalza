214a09c404cc5fd4ff522f7f60dd9c4bbb8fae96
sujin cho <sujinemilycho@gmail.com>
2021-03-04 23:11:00 +0900

    chomily: 2021/3/4 프로그래머스 완전 탐색 2문제
    - 소수 찾기
    - 카펫
    - 파일, 디렉토리 이름 수

---
+++ 프로그래머스/완전탐색/카펫/chomily.java
@@ -0,0 +1,11 @@
+public class chomily {
+    public int[] solution(int brown, int yellow) {
+        for(int y = 1; y <= Math.sqrt(yellow); y++) {
+            double x = yellow * 1.0 / y;
+            if(2 * (x + y + 2) == brown) {
+                return new int[]{(int)x, y};
+            }
+        }
+        throw new IllegalStateException();
+    }
+}
