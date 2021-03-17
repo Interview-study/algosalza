edc58a2125359e23eb1c5b0be19ddac4e067ee9d
hj <a301dks@naver.com>
2021-03-17 19:26:34 +0900

    update 더 맵게

--- 프로그래머스/힙(Heap)/더_맵게/sika.java
+++ 프로그래머스/힙(Heap)/더_맵게/sika.java
@@ -1,7 +1,9 @@
 package programmers;
 
-import java.util.Objects;
-import java.util.TreeSet;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class heap1 {
     public static void main(String[] args) {
@@ -9,65 +11,26 @@ public class heap1 {
     }
 
     public static int solution(int[] scoville, int K) {
-        final TreeSet<Food> minHeap = new TreeSet<>();
-        for (int i = 0; i < scoville.length; i++) {
-            minHeap.add(new Food(scoville[i], i));
-        }
+        final Queue<Integer> minHeap = Arrays.stream(scoville)
+            .boxed()
+            .collect(Collectors.toCollection(PriorityQueue::new));
 
         int answer = 0;
-        int idx = scoville.length;
         while (true) {
-            final Food first = minHeap.pollFirst();
-            if (first.scoville >= K) {
+            final int first = minHeap.remove();
+            if (first >= K) {
                 return answer;
             }
-            if (first.scoville < K && minHeap.isEmpty()) {
+            if (minHeap.isEmpty()) {
                 return -1;
             }
-            final Food second = minHeap.pollFirst();
-            minHeap.add(new Food(calculateScoville(first.scoville, second.scoville), idx++));
+            final int second = minHeap.remove();
+            minHeap.add(calculateScoville(first, second));
             answer++;
         }
     }
 
     public static int calculateScoville(final int first, final int second) {
         return first + (second * 2);
-    }
-
-    private static class Food implements Comparable<Food> {
-        final int scoville;
-        final int idx;
-
-        public Food(final int scoville, final int idx) {
-            this.scoville = scoville;
-            this.idx = idx;
-        }
-
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o)
-                return true;
-            if (o == null || getClass() != o.getClass())
-                return false;
-            final Food food = (Food)o;
-            return scoville == food.scoville &&
-                idx == food.idx;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(scoville, idx);
-        }
-
-        @Override
-        public int compareTo(final Food that) {
-            if (this.scoville < that.scoville) {
-                return -1;
-            } else if (this.scoville == that.scoville) {
-                return Integer.compare(this.idx, that.idx);
-            } else {
-                return 1;
-            }
-        }
     }
 }
